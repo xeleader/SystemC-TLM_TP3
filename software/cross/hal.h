910243ab137b72827e8313509e050ce6275080ad
Alexandre Blervaque <Alexandre.Blervaque@phelma.grenoble-inp.fr>
2017-12-22 09:04:28 +0100

    Rappatriement des fichiers

---
+++ software/cross/hal.h
@@ -0,0 +1,39 @@
+/********************************************************************
+ * Copyright (C) 2009, 2012 by Verimag                              *
+ * Initial author: Matthieu Moy                                     *
+ ********************************************************************/
+
+/*!
+  \file hal.h
+  \brief Harwdare Abstraction Layer : implementation for MicroBlaze
+  ISS.
+
+
+*/
+#ifndef HAL_H
+#define HAL_H
+
+#include <stdint.h>
+
+
+/* Dummy implementation of abort(): invalid instruction */
+#define abort() do {				\
+	printf("abort() function called\r\n");  \
+	_hw_exception_handler();		\
+} while (0)
+
+/* TODO: implement HAL primitives for cross-compilation */
+#define hal_read32(a)      abort()
+#define hal_write32(a, d)  abort()
+#define hal_wait_for_irq() abort()
+#define hal_cpu_relax()    abort()
+
+void microblaze_enable_interrupts(void) {
+	__asm("ori     r3, r0, 2\n"
+	      "mts     rmsr, r3");
+}
+
+/* TODO: printf is disabled, for now ... */
+#define printf(...) do {} while(0)
+
+#endif /* HAL_H */
